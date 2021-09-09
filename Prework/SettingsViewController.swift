7d37455cd6f4b1701e89d1dd689278ff852c053d
ElioZhou <41265059+ElioZhou@users.noreply.github.com>
2021-09-08 22:27:49 -0400

    Version 1

---
+++ Prework/SettingsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by 周宇振 on 2021/9/8.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        print("view will appear")
+//        // This is a good place to retrieve the default tip percentage from UserDefaults
+//        // and use it to update the tip amount
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        print("view did appear")
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        print("view will disappear")
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        print("view did disappear")
+//    }
+//
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -11,21 +11,54 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipSlider: UISlider!
     @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        billAmountTextField.becomeFirstResponder()
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func calculateTip(_ sender: Any) {
+    func calculateTipSlider(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
-        let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+//        let tipPercentages = [0.15, 0.18, 0.2]
+//        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentage = Double(tipSlider.value)/100.0
+        let tip = bill * tipPercentage
         let total = bill + tip
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
         
+    }
+    
+    func calculateTipSelector(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+//        let tip = bill * tipPercentages
+        let total = bill + tip
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func tipSelector(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+//        let tip = bill * tipPercentages
+        let total = bill + tip
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func tipSlider(_ sender: Any) {
+        tipPercentageLabel.text = String(format: "Tip: %02d%%", Int(tipSlider.value))
+        calculateTipSlider((Any).self)
     }
     
 }
